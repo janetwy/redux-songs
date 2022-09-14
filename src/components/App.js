2bc7e13c6beaaf49cc736a091358fa6ee624d7ec
Janet Tang <tang.j56@gmail.com>
2022-09-13 19:32:20 -0500

    add basic components

---
+++ src/components/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import SongDetail from "./SongDetail";
+import SongList from "./SongList";
+
+const App = () => {
+    return (
+        <h1>hello world</h1>
+    );
+};
+
+export default App;
